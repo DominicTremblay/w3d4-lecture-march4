195784b499dad5b90300c18a10e6baa3a923917b
Dominic Tremblay <dominictremblay24@gmail.com>
2019-03-21 14:52:13 -0400

    added readme

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,103 @@
-$(document).ready(function() {}); // document ready
+// Creating a reusable Ajax request with a callback
+const request = (options, cb) => {
+  $.ajax(options)
+    .done(response => cb(response))
+    .fail(err => console.log(`Err: ${err}`))
+    .always(() => console.log('Request completed.'));
+};
+
+// Issue a request to get the list of quotes from the backend server
+const loadQuotes = () => {
+  // Options for the request
+  const reqOptions = {
+    url: '/quotes',
+    method: 'GET',
+    dataType: 'json',
+  };
+
+  const createQuoteEl = quoteObj => {
+    return `<div class="card">
+  <div class="card-header" id="${quoteObj._id}">
+    <h5 class="mb-0">
+      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${
+        quoteObj._id
+      }" aria-expanded="true" aria-controls="${
+      quoteObj._id
+    }" style="visibility: visible;">${quoteObj.quote}</button>
+      <div>
+        <form class="quote-update-frm" style="visibility: hidden;">
+          <input type="text" name="editquote" class="edit-quote" value="${
+            quoteObj.quote
+          }">
+          <input type="submit" class="btn-small btn-primary" value="Update">
+        </form>
+      </div>
+    </h5>
+
+    <span>
+      <form class="quote-edit-frm"><input type="submit" class="btn btn-secondary btn-sm" value="Edit"></form>
+      <form class="quote-delete-frm"><input type="submit" class="btn btn-secondary btn-sm" value="Delete"></form>
+    </span>
+
+  </div>
+</div>`;
+  };
+
+  // loop through the array of quote objects
+  // inside the loop, create each HTML element of each quote
+  // adding each quote HTML element to the DOM
+  const renderQuotes = quotesArr => {
+    // Need to empty the container, in case render is called many times
+    $('#quote-list').empty();
+    for (const quoteObj of quotesArr) {
+      // create an HTML element out of quoteObj
+      const quoteEl = createQuoteEl(quoteObj);
+      // Add the element to the DOM
+      $('#quote-list').prepend(quoteEl);
+    }
+  };
+
+  // calling the request function with the options and callback
+  request(reqOptions, function(quotesArr) {
+    // loop through the array of quote objects
+    renderQuotes(quotesArr);
+  });
+};
+
+$(document).ready(function() {
+  // When clicking add button, issue a request to create a new quote
+  $('#add-quote-frm').on('submit', function(event) {
+    event.preventDefault();
+
+    // Extract the content of the input box
+    // Use DOM traversal to get the content
+    const quoteContent = $(this)
+      .find('input[type=text]')
+      .val();
+
+    // Do some king of validation but not with alerts! :)
+    if (!quoteContent) {
+      alert('Put something in the box!');
+    } else {
+      // Creating the request options for creating a new quote
+      const reqOptions = {
+        url: '/quotes',
+        method: 'POST',
+        data: { quote: quoteContent },
+      };
+
+      // sending the ajax POST request to the backend
+      request(reqOptions, function(quoteObj) {
+        console.log(quoteObj);
+
+        // If we want to add only the new quote
+        renderQuotes(quote);
+
+        // If we want to reload all the quotes
+        // loadQuotes();
+      });
+    }
+  });
+
+  loadQuotes();
+}); // document ready
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const ObjectID = require('mongodb').ObjectID;
 const port = process.env.PORT || 8000;
 const app = express();
 const MongoClient = Mongo.MongoClient;
+
+// URL to connect to the mongoDB, default port 27017 on localhost
 const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/movieQuotes';
 
@@ -14,6 +16,7 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Have a db instance in the global scope
 let db = null;
 
 // Establish connection to the mongo db and assign the mongoDb object to db
@@ -23,7 +26,41 @@ MongoClient.connect(MONGODB_URI, (err, mongoDb) => {
     process.exit(1);
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
+  // assign mongoDB (database instance) to the global db variable
   db = mongoDb;
+});
+
+// Create the routes to respond to request
+
+// Get the quote list from the database
+
+app.get('/quotes', (req, res) => {
+  // creating the query to get all the quotes from mongo db
+  // request is asynchronous
+
+  db.collection('quotes')
+    .find()
+    .toArray()
+    .then(data => res.json(data))
+    .catch(error => res.send(error));
+});
+
+// Creating a post route to create a new quote
+
+app.post('/quotes', (req, res) => {
+  // extract the content from que request
+  const { quote } = req.body;
+
+  // Crating the structure of the data that needs to be inserted into the database
+  const quoteObj = {
+    quote: quote,
+    comments: [],
+  };
+  // create an insert query
+  db.collection('quotes')
+    .insertOne(quoteObj)
+    .then(response => res.json(quoteObj))
+    .catch(err => res.send(err));
 });
 
 app.listen(port, () => {
